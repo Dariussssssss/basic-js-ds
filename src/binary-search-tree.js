3458b47ce3ca969baf6add336599f74093fbd1be
Dariusssssssss <sablezzubaya@gmail.com>
2025-01-07 02:20:42 +0300

    add: task solution "binary search tree"

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,44 +6,119 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
-class BinarySearchTree {
-
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
   }
 }
-
+class BinarySearchTree {
+  constructor() {
+    this.rootNode = null;
+  }
+  root() {
+    return this.rootNode;
+  }
+  add(data) {
+    const newNode = new Node(data);
+    if (!this.rootNode) {
+      this.rootNode = newNode;
+    } else {
+      this._addNode(this.rootNode, newNode);
+    }
+  }
+  _addNode(currentNode, newNode) {
+    if (newNode.data < currentNode.data) {
+      if (!currentNode.left) {
+        currentNode.left = newNode;
+      } else {
+        this._addNode(currentNode.left, newNode);
+      }
+    } else {
+      if (!currentNode.right) {
+        currentNode.right = newNode;
+      } else {
+        this._addNode(currentNode.right, newNode);
+      }
+    }
+  }
+  has(data) {
+    return this._hasNode(this.rootNode, data);
+  }
+  _hasNode(currentNode, data) {
+    if (!currentNode) {
+      return false;
+    }
+    if (data < currentNode.data) {
+      return this._hasNode(currentNode.left, data);
+    } else if (data > currentNode.data) {
+      return this._hasNode(currentNode.right, data);
+    } else {
+      return true; // data is equal to currentNode.data
+    }
+  }
+  find(data) {
+    return this._findNode(this.rootNode, data);
+  }
+  _findNode(currentNode, data) {
+    if (!currentNode) {
+      return null;
+    }
+    if (data < currentNode.data) {
+      return this._findNode(currentNode.left, data);
+    } else if (data > currentNode.data) {
+      return this._findNode(currentNode.right, data);
+    } else {
+      return currentNode;
+    }
+  }
+  remove(data) {
+    this.rootNode = this._removeNode(this.rootNode, data);
+  }
+  _removeNode(currentNode, data) {
+    if (!currentNode) {
+      return null;
+    }
+    if (data < currentNode.data) {
+      currentNode.left = this._removeNode(currentNode.left, data);
+      return currentNode;
+    } else if (data > currentNode.data) {
+      currentNode.right = this._removeNode(currentNode.right, data);
+      return currentNode;
+    } else {
+      if (!currentNode.left && !currentNode.right) {
+        return null;
+      }
+      if (!currentNode.left) {
+        return currentNode.right;
+      } else if (!currentNode.right) {
+        return currentNode.left;
+      }
+      const minNode = this._findMinNode(currentNode.right);
+      currentNode.data = minNode.data;
+      currentNode.right = this._removeNode(currentNode.right, minNode.data);
+      return currentNode;
+    }
+  }
+  _findMinNode(currentNode) {
+    while (currentNode && currentNode.left) {
+      currentNode = currentNode.left;
+    }
+    return currentNode;
+  }
+  min() {
+    const minNode = this._findMinNode(this.rootNode);
+    return minNode ? minNode.data : null;
+  }
+  max() {
+    let currentNode = this.rootNode;
+    while (currentNode && currentNode.right) {
+      currentNode = currentNode.right;
+    }
+    return currentNode ? currentNode.data : null;
+  }
+}
 module.exports = {
   BinarySearchTree
 };
